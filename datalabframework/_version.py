51f2bf49783506047ec10e6d44650ba6b28b553c
natbusa <natalino.busa@gmail.com>
2018-09-20 17:17:16 +0700

    version bump

--- datalabframework/_version.py
+++ datalabframework/_version.py
@@ -1,6 +1,6 @@
 # Copyright (c) Jupyter Development Team.
 # Distributed under the terms of the Modified BSD License.
 
-version_info = (0, 3, 10)
+version_info = (0, 3, 11)
 
 __version__ = '.'.join(map(str, version_info))
