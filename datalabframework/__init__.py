f11e1d264613cb2da92a725f513dbfaa21c42286
natbusa <natalino.busa@gmail.com>
2018-10-11 08:17:19 +0700

    log -> logging

--- datalabframework/__init__.py
+++ datalabframework/__init__.py
@@ -1,11 +1,21 @@
 # automatically imports submodules
-from . import log
+from . import logging
 from . import notebook
 from . import params
 from . import data
 from . import engines
 from . import project
 
-__all__ = ['log', 'notebook', 'project', 'params', 'data', 'engines']
+from ._version import version_info, __version__
+
+#public submodules
+__all__ = [
+    'logging',
+    'notebook',
+    'project',
+    'params',
+    'data',
+    'engines'
+]
 
 __DATALABFRAMEWORK__=True
--- datalabframework/_version.py
+++ datalabframework/_version.py
@@ -1,6 +1,2 @@
-# Copyright (c) Jupyter Development Team.
-# Distributed under the terms of the Modified BSD License.
-
-version_info = (0, 5, 2)
-
+version_info = (0, 5, 3)
 __version__ = '.'.join(map(str, version_info))
