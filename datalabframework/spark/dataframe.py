19976c6f81c857efa75e413f1cbcd6d8b29a95e6
Natalino Busa <natbusa@teko.vn>
2019-02-11 10:07:23 +0700

    return whole df when no update and state column availble

--- datalabframework/spark/dataframe.py
+++ datalabframework/spark/dataframe.py
@@ -82,6 +82,12 @@ def view(df, colnames=None, state_col='_state', updated_col='_updated'):
     c = set(df.columns).difference({state_col, updated_col})
     colnames = [x for x in df.columns if x in c] if colnames is None else colnames
 
+    if updated_col not in colnames:
+        return df
+    
+    if state_col not in colnames:
+        return df
+    
     row_groups = df.groupBy(colnames)
     df_view = row_groups.agg(F.sort_array(F.collect_list(F.struct( F.col(updated_col), F.col(state_col))),asc = False).getItem(0).alias('_last')).select(*colnames, '_last.*')
     df = df_view.filter("{} = 0".format(state_col))
